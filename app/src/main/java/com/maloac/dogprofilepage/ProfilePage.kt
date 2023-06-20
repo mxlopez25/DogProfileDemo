887f4ca847b4cdaaed5be1f64ba5a6f57639bd36
Maximo Lopez <7888441+mxlopez25@users.noreply.github.com>
2023-06-20 18:28:43 -0400

    ConstraintLayout implemented in ProfilePage

--- app/src/main/java/com/maloac/dogprofilepage/ProfilePage.kt
+++ app/src/main/java/com/maloac/dogprofilepage/ProfilePage.kt
@@ -3,69 +3,110 @@ package com.maloac.dogprofilepage
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 
 @Composable
 fun ProfilePage() {
-    Card(elevation = 10.dp,
-    modifier = Modifier
-        .fillMaxSize()
-        .padding(start = 16.dp, end = 16.dp, top = 100.dp, bottom = 100.dp)
-        .border(width = 3.dp, color = Color.Red, shape = RoundedCornerShape(30.dp))) {
-        Column(
+    Card(
+        elevation = 10.dp,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp, top = 100.dp, bottom = 100.dp)
+            .border(width = 3.dp, color = Color.Red, shape = RoundedCornerShape(30.dp))
+    ) {
+        ConstraintLayout {
+            val (image,
+                nameText,
+                countryText,
+                profileStats,
+                buttonFollow,
+                buttonDirectMessage
+            ) = createRefs()
 
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.verticalScroll(rememberScrollState())
-        ) {
-            Image(painter = painterResource(id = R.drawable.barber),
+            val guideLine = createGuidelineFromTop(fraction = 0.05f)
+
+            Image(
+                painter = painterResource(id = R.drawable.barber),
                 contentDescription = "Logo",
                 modifier = Modifier
                     .size(100.dp)
                     .clip(CircleShape)
-                    .border(width = 3.dp, color = Color.Red, shape = CircleShape),
+                    .border(width = 3.dp, color = Color.Red, shape = CircleShape)
+                    .constrainAs(image) {
+                        top.linkTo(guideLine)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
                 contentScale = ContentScale.Crop
             )
-            Text(text = "Barber")
-            Text(text = "Dominican Republic")
 
-            Row(horizontalArrangement = Arrangement.SpaceEvenly,
+            Text(text = "Barber",
+                modifier = Modifier.constrainAs(nameText) {
+                    top.linkTo(image.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+
+            Text(text = "Dominican Republic",
+                modifier = Modifier.constrainAs(countryText) {
+                    top.linkTo(nameText.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(15.dp)) {
+                    .padding(15.dp)
+                    .constrainAs(profileStats) {
+                        top.linkTo(countryText.bottom)
+                    }
+            ) {
                 ProfileStats("Followers", "300")
                 ProfileStats("Following", "150")
                 ProfileStats("Posts", "40")
             }
-            Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)) {
-                Button(onClick = {}) {
-                    Text(text = "Follow user")
-                }
 
-                Button(onClick = {}) {
-                    Text(text = "Direct Menssage")
+            Button(onClick = {},
+                modifier = Modifier.constrainAs(buttonFollow) {
+                    top.linkTo(profileStats.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(buttonDirectMessage.start)
+                    width = Dimension.wrapContent
                 }
+                ) {
+                Text(text = "Follow user")
             }
+
+            Button(onClick = {},
+                modifier = Modifier.constrainAs(buttonDirectMessage) {
+                    top.linkTo(profileStats.bottom)
+                    start.linkTo(buttonFollow.end)
+                    end.linkTo(parent.end)
+                }
+                ) {
+                Text(text = "Direct Menssage")
+            }
+
         }
     }
 }
